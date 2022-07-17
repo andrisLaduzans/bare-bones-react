3486e7a3600fc161d2e124661518289d5b2b439d
Andris Laduzans <andris.laduzans@gmail.com>
2022-07-17 15:22:11 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const App = () => {
+    return (
+      <div>
+        <h1>Hello!!</h1>
+        <h2>Welcome to your First React App..!</h2>
+      </div>
+    );
+  };
